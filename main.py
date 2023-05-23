7fa97213d3389e4992b687b51e3a348de9e52ccb
nivjk <134269336+nivjk@users.noreply.github.com>
2023-05-23 16:21:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,45 @@ from sort import Sort
 # 
 # main
 
+def init_sorcier(sorcier, name):
+    set_name = SetName()
+    get_name = GetName()
+    fly = Fly()
+    sort = Sort()
+
+    set_name.set_sorcier(sorcier)
+    get_name.set_sorcier(sorcier)
+    fly.set_sorcier(sorcier)
+    sort.set_sorcier(sorcier)
+
+    set_name.set_name(name)
+
+    name = get_name.get_name()
+    print(f"Nom du sorcier : {name}")
+
+    fly.activer_le_vol()
+
+    sort.activer_les_sorts()
+
+    sort.activer_les_sorts_maudits()
+
+    sort.activer_les_sorts_vegetal()
+
+    print(f"Peut voler : {sorcier.peut_voler}")
+    print(f"Peut lancer des sorts : {sorcier.peut_lancer_des_sorts}")
+    print(f"Peut lancer des sorts maudits : {sorcier.est_maudit}")
+    print(f"Peut manier les plantes : {sorcier.peut_lancer_des_sorts_vegetal}")
+
+    reponse = input(f"\nVoulez-vous retirer l'utilisation des sorts maudits à {name} ? (o / n) ")
+
+    if reponse == "o":
+        print(f"\nVous lancez un sort à {name}...")
+        print(f"{name} vous a lancé un sort en retour !!!")
+
+    else:
+        print(f"Suite à votre réponse, {name} se mit à rire.")
+
+
 class Sorcier:
     def __init__(self):
         self.name = None
@@ -20,82 +59,9 @@ class Sorcier:
 
 if __name__ == "__main__":
     harry = Sorcier()
-
-    set_name = SetName()
-    get_name = GetName()
-    fly = Fly()
-    sort = Sort()
-
-    set_name.set_sorcier(harry)
-    get_name.set_sorcier(harry)
-    fly.set_sorcier(harry)
-    sort.set_sorcier(harry)
-
-    set_name.set_name("Harry Potter")
-
-    name = get_name.get_name()
-    print(f"Nom du sorcier : {name}")
-
-    fly.activer_le_vol()
-
-    sort.activer_les_sorts()
-
-    sort.activer_les_sorts_maudits()
-
-    sort.activer_les_sorts_vegetal()
-
-    print(f"Peut voler : {harry.peut_voler}")
-    print(f"Peut lancer des sorts : {harry.peut_lancer_des_sorts}")
-    print(f"Peut lancer des sorts maudits : {harry.est_maudit}")
-    print(f"Peut manier les plantes : {harry.peut_lancer_des_sorts_vegetal}")
-
-    reponse = input(f"\nVoulez-vous retirer l'utilisation des sorts maudits à {name} ? (o \ n) ")
-
-    if reponse == "o" :
-        print(f"\nVous lancez un sort à {name}...")
-        sort.desactiver_les_sorts_maudits()
-        print(f"{name} peut lancer des sorts maudits : {harry.est_maudit}")
-        print(f"{name} n'est plus maudit.\n\n")
-    else :
-        print(f"Suite à votre réponse, {name} se mit à vous attaquer.")
-
+    init_sorcier(harry, "Harry Potter")
 
     print("\n\n------------------------------------------------\n\n")
 
     bellatrix = Sorcier()
-    set_name = SetName()
-    get_name = GetName()
-    fly = Fly()
-    sort = Sort()
-
-    set_name.set_sorcier(bellatrix)
-    get_name.set_sorcier(bellatrix)
-    fly.set_sorcier(bellatrix)
-    sort.set_sorcier(bellatrix)
-
-    set_name.set_name("Bellatrix")
-
-    name = get_name.get_name()
-    print(f"Nom du sorcier : {name}")
-
-    fly.activer_le_vol()
-
-    sort.activer_les_sorts()
-
-    sort.activer_les_sorts_maudits()
-
-    sort.activer_les_sorts_vegetal()
-
-    print(f"Peut voler : {bellatrix.peut_voler}")
-    print(f"Peut lancer des sorts : {bellatrix.peut_lancer_des_sorts}")
-    print(f"Peut lancer des sorts maudits : {bellatrix.est_maudit}")
-    print(f"Peut manier les plantes : {bellatrix.peut_lancer_des_sorts_vegetal}")
-
-    reponse = input(f"\nVoulez-vous retirer l'utilisation des sorts maudits à {name} ? (o \ n) ")
-
-    if reponse == "o" :
-        print(f"\nVous lancez un sort à {name}...")
-        print(f"{name} vous a lancé un sort en retour !!!")
-
-    else :
-        print(f"Suite à votre réponse, {name} se mit à rire.")
+    init_sorcier(bellatrix, "Bellatrix")
